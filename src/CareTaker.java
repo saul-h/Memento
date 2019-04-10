589834ea599e43222783bf73121a80103bba4bb7
Abel Acosta <abelacosta1596@gmail.com>
2019-04-09 17:33:48 -0700

    Defined methods and instance variables

--- src/CareTaker.java
+++ src/CareTaker.java
@@ -1,3 +1,17 @@
-public class CareTaker {
+import java.util.ArrayList;
 
+public class CareTaker {
+	private ArrayList<Memento> mementos;
+	
+	public CareTaker() {
+		mementos = new ArrayList<Memento>();
+	}
+	
+	public void add(Memento state) {
+		mementos.add(state);
+	}
+	
+	public Memento get(int index) {
+		return mementos.get(index);
+	}
 }
--- src/Originator.java
+++ src/Originator.java
@@ -1,3 +1,11 @@
 public class Originator {
-
+	AdvancedIceCreamCone iceCream;
+	
+	public void setState(AdvancedIceCreamCone iceCream) {
+		this.iceCream = iceCream;
+	}
+	
+	public Memento saveStateToMemento(){
+	      return new Memento(iceCream.getNumberOfScoops(), iceCream.getFlavor(), iceCream.getTypeOfCone());
+	}
 }
