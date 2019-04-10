6bb8e8fce925363be811e55bd6d57b177d8457e4
Abel Acosta <abelacosta1596@gmail.com>
2019-04-09 17:22:34 -0700

    Defined Memento class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+/*
+ * Abel Acosta
+ * Saul Hernandez
+ */
+
 public class Main {
 	public static void main(String [] args){
-
+		// Creating ice creams
+		AdvancedIceCreamCone chocolateIceCream = new AdvancedIceCreamCone(2, "Chocolate", "Regular");
+		AdvancedIceCreamCone vanillaIceCream = new AdvancedIceCreamCone(1, "Vanilla", "Waffle");
+		AdvancedIceCreamCone strawberryIceCream = new AdvancedIceCreamCone(4, "Strawberry", "Sugar");
+		
+		// TODO: Save state of ice creams
 	}
 }
--- src/Memento.java
+++ src/Memento.java
@@ -1,3 +1,29 @@
 public class Memento {
-
+	
+	private int numberOfScoops;
+	private String flavor;
+	private String typeOfCone;
+	
+	public Memento(int numberOfScoops, String flavor, String typeOfCone) {
+		this.numberOfScoops = numberOfScoops;
+		this.flavor = flavor;
+		this.typeOfCone = typeOfCone;
+	}
+	
+	public int getNumberOfScoops() {
+		return numberOfScoops;
+	}
+	
+	public String getFlavor() {
+		return flavor;
+	}
+	
+	public String typeOfCone() {
+		return typeOfCone;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("State: %5d%10s%10s", numberOfScoops, flavor, typeOfCone);
+	}
 }
