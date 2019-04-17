1f0b7f7809a8e733f05153c1636a0d2b66a26e05
saul-h <saulhernandez217@gmail.com>
2019-04-17 13:06:59 -0700

    Added toppings

--- src/Memento.java
+++ src/Memento.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
 public class Memento {
 	
 	private int numberOfScoops;
 	private String flavor;
 	private String typeOfCone;
+	private ArrayList<String> toppings;
 	
-	public Memento(int numberOfScoops, String flavor, String typeOfCone) {
+	public Memento(int numberOfScoops, String flavor, String typeOfCone, ArrayList<String> toppings) {
 		this.numberOfScoops = numberOfScoops;
 		this.flavor = flavor;
 		this.typeOfCone = typeOfCone;
+		this.toppings = toppings;
 	}
 	
 	public int getNumberOfScoops() {
@@ -20,6 +23,10 @@ public class Memento {
 	
 	public String typeOfCone() {
 		return typeOfCone;
+	}
+	
+	public ArrayList<String> getToppings() {
+		return toppings;
 	}
 	
 	@Override
